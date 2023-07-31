d53c461dcaa8d5c5ed824623f5c281b0ab554236
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2023-07-31 07:05:38 +0100

    Installer Updated
    Now requests user input if multiple versions of the game are detected.

--- Expanded Galaxy Main Installer/use-this-install-please.bat
+++ Expanded Galaxy Main Installer/use-this-install-please.bat
@@ -1,12 +1,13 @@
 title Expanded Galaxy Project Installer
 
 @echo off
+set temp = 0
 
 Rem KOTOR2 DISC
 reg query HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\LucasArts\KotOR2 /v Path /s
 if %errorlevel% equ 0 (
   echo "Hello World! Loyal fan & owner of a disc copy of the game!"
-  FOR /F "tokens=2* skip=2" %%a in ('reg query "HKLM\SOFTWARE\WOW6432Node\LucasArts\KotOR2" /v "Path"') do set mykey2=%%b
+  FOR /F "tokens=2* skip=2" %%a in ('reg query "HKLM\SOFTWARE\WOW6432Node\LucasArts\KotOR2" /v "Path"') do set mykey1=%%b
 ) else (
   Rem echo "kotor 2 disc registry entry not detected!"
 )
@@ -24,31 +25,62 @@ Rem KOTOR2 STEAM
 reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 208580" /v InstallLocation /s
 if %errorlevel% equ 0 (
   echo "Hello World! Steam User!"
-  FOR /F "tokens=2* skip=2" %%a in ('reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 208580" /v "InstallLocation"') do set mykey2=%%b
+  FOR /F "tokens=2* skip=2" %%a in ('reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 208580" /v "InstallLocation"') do set mykey3=%%b
 ) else (
   Rem echo "kotor 2 steam registry entry not detected!"
 )
 
 cls
 
-if defined mykey2 (
-echo "Star Wars : Knights of the Old Republic II Installation Detected!"
-) else (
-echo "Star Wars : Knights of the Old Republic II Installation Not Detected!"
-pause
-exit
+if defined mykey1 (
+  set /a temp=%temp%+1
 )
 
-if exist "%mykey2%\chitin.key" (
-  Rem echo Game Found.
+if defined mykey2 (
+  set /a temp=%temp%+1
+)
+
+if defined mykey3 (
+  set /a temp=%temp%+1
+)
+
+if temp == 0 goto INSTALL
+
+:INPUT
+echo Please enter which install of the first game to use.
+echo Disc Version = 1
+echo GoG Version = 2
+echo Steam Version = 3
+set /p Install="Which install to use: "
+echo %Install%|findstr /r "[^0-9]" && (
+    goto :INPUT
+)
+if %Install% gtr 3  (
+   echo enter a number between 1 and 3
+   goto :INPUT
+)
+if %Install% lss 1 (
+   echo enter a number between 1 and 3
+   goto :INPUT
+)
+
+if %Install% == 1 set mykey=%mykey1%
+if %Install% == 2 set mykey=%mykey2%
+if %Install% == 3 set mykey=%mykey3%
+
+:INSTALL
+
+if exist "%mykey%\chitin.key" (
+  echo "Star Wars : Knights of the Old Republic II Installation Detected!"
 ) else (
-  Rem echo Game Not Found!
+  echo "Star Wars : Knights of the Old Republic II Installation Not Detected!"
+  pause
   exit
 )
 
 echo "Installing Expanded Galaxy Project"
 
-tslpatchdata\pykotorcli.exe "%mykey2%" "%cd%"
+tslpatchdata\pykotorcli.exe "%mykey%" "%cd%"
 
 cls
 
