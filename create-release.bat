977bcf61107ba8a568d5f2db1244c62f06db3596
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2024-01-28 22:25:39 +0000

    release script fix

--- create-release.bat
+++ create-release.bat
@@ -2,7 +2,7 @@
 
 for /f "delims=" %%x in (version.txt) do set Build=main-v%%x
 
-"C:\Program Files\7-Zip\7z.exe" a "%Build%.7z" "%cd%\Expanded Galaxy Main Installer\*"
+"C:\Program Files\7-Zip\7z.exe" a "%Build%.zip" "%cd%\Expanded Galaxy Main Installer\*"
 
 set fld=Movies
 call :ADD
@@ -13,7 +13,7 @@ md tslpatchdata
 for /r "Modules" %%x in (*.mod) do copy "%%x" "tslpatchdata\"
 for /r "StreamVoice" %%x in (*.wav) do copy "%%x" "tslpatchdata\"
 for /r "StreamVoice" %%x in (*.mp3) do copy "%%x" "tslpatchdata\"
-"C:\Program Files\7-Zip\7z.exe" a "%Build%.7z" "%cd%\tslpatchdata"
+"C:\Program Files\7-Zip\7z.exe" a "%Build%.zip" "%cd%\tslpatchdata"
 rd /s /q tslpatchdata
 
 echo release zipped
@@ -22,6 +22,6 @@ exit
 
 :ADD
 ren %fld% tslpatchdata
-"C:\Program Files\7-Zip\7z.exe" a "%Build%.7z" "%cd%\tslpatchdata"
+"C:\Program Files\7-Zip\7z.exe" a "%Build%.zip" "%cd%\tslpatchdata"
 ren tslpatchdata %fld%
 exit /b
