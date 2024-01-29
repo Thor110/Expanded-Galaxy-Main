0cb10060ac795e3fc6225400d7138946cb2199d9
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2024-01-29 00:59:24 +0000

    Modules SourceFolder

--- create-release.bat
+++ create-release.bat
@@ -5,7 +5,7 @@ for /f "delims=" %%x in (version.txt) do set Build=main-v%%x
 "C:\Program Files\7-Zip\7z.exe" a "%Build%.zip" "%cd%\Expanded Galaxy Main Installer\*"
 
 md tslpatchdata
-for /r "Modules" %%x in (*.mod) do copy "%%x" "tslpatchdata\"
+for /r "Modules" %%x in (*.mod) do copy "%%x" "tslpatchdata\Modules"
 "C:\Program Files\7-Zip\7z.exe" a "%Build%.zip" "%cd%\tslpatchdata"
 rd /s /q tslpatchdata
 
