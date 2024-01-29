34cabc90f28c7d6858c082213bfd568001a17179
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2024-01-29 01:46:15 +0000

    echo update

--- update-modules.bat
+++ update-modules.bat
@@ -1,11 +1,7 @@
 @echo off
 
-for /f "delims=" %%x in (version.txt) do set Build=main-v%%x
-
 for /r "Modules Source" %%x in (*.mod) do copy "%%x" "%cd%\Expanded Galaxy Main Installer\tslpatchdata\Modules"
 
-"C:\Program Files\7-Zip\7z.exe" a "%Build%.zip" "%cd%\Expanded Galaxy Main Installer\*"
-
-echo release zipped
+echo Modules Updated
 pause
 exit
