21a33fc67560ed1f99cfc21bd72ada6fbb74a6ee
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2024-05-11 07:58:25 +0100

    nwnnsscomp included

--- compile-all-nss-in-all-sub-directories.bat
+++ compile-all-nss-in-all-sub-directories.bat
@@ -1,10 +1,8 @@
 @echo off
 
-set nwnsscompdir=C:\Program Files (x86)\KotOR Tools\TSLPatcher1210b1
-
 for /r %%F in (./*.nss) do (
   if not "%%~nF"=="nwscript" (
-    "%nwnsscompdir%\nwnnsscomp.exe" -c "%%~fF" -o "%%~npF.ncs"
+    "nwnnsscomp.exe" -c "%%~fF" -o "%%~npF.ncs"
   ) else (
     echo Skipping: %%F
   )
