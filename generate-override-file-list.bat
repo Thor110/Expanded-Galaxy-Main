a51f6c7adff3d88c41fdb8ad0ed41fad2229bddb
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2024-11-01 12:50:00 +0000

    Automatically Update Changes Override List

--- generate-override-file-list.bat
+++ generate-override-file-list.bat
@@ -2,9 +2,13 @@
 rem Thor110
 rem generate override file list for changes.ini
 
-if exist override.txt (
-	del override.txt
-)
+del "Expanded Galaxy Main Installer\tslpatchdata\changes-amazon.ini"
+del "Expanded Galaxy Main Installer\tslpatchdata\changes-android.ini"
+del "Expanded Galaxy Main Installer\tslpatchdata\changes-disc.ini"
+del "Expanded Galaxy Main Installer\tslpatchdata\changes-gog.ini"
+del "Expanded Galaxy Main Installer\tslpatchdata\changes-legacypc.ini"
+del "Expanded Galaxy Main Installer\tslpatchdata\changes-steam.ini"
+del "Expanded Galaxy Main Installer\tslpatchdata\changes-xbox.ini"
 
 dir "Expanded Galaxy Main Installer\tslpatchdata\Override" /b > temp-list.txt
 
@@ -18,8 +22,29 @@ for /f "delims=|" %%i in (temp-list.txt) do (
   set /a x=!x!+1
 )
 
+REM echo.>> override.txt
+REM echo ; ===================================================================>> override.txt
+
 del temp-list.txt
 
 echo Replace List generated as override.txt
+
+echo Copying changes.ini files...
+
+for /f %%f in ('dir /b changes') do copy /y "changes\%%f" "Expanded Galaxy Main Installer\tslpatchdata\%%f"
+
+echo updating changes.ini files...
+
+for /f "delims=|" %%o in (override.txt) do (
+  echo %%o>> "Expanded Galaxy Main Installer\tslpatchdata\changes-amazon.ini"
+  echo %%o>> "Expanded Galaxy Main Installer\tslpatchdata\changes-android.ini"
+  echo %%o>> "Expanded Galaxy Main Installer\tslpatchdata\changes-disc.ini"
+  echo %%o>> "Expanded Galaxy Main Installer\tslpatchdata\changes-gog.ini"
+  echo %%o>> "Expanded Galaxy Main Installer\tslpatchdata\changes-legacypc.ini"
+  echo %%o>> "Expanded Galaxy Main Installer\tslpatchdata\changes-steam.ini"
+  echo %%o>> "Expanded Galaxy Main Installer\tslpatchdata\changes-xbox.ini"
+)
+
+del override.txt
 
 pause
