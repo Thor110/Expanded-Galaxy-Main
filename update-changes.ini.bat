4d53dd289d9f038c2789c2451190df878f3ea0af
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2024-11-01 17:03:09 +0000

    Type > Copy

--- update-changes.ini.bat
+++ update-changes.ini.bat
@@ -32,14 +32,14 @@ echo Override Install List Generated!
 
 echo Creating changes.ini files...
 
-copy changes\changes-amazon-intro.ini		+ changes\changes.ini + changes\changes-amazon-outro.ini		+ override.txt "Expanded Galaxy Main Installer\tslpatchdata\changes-amazon.ini"
-copy changes\changes-android-intro.ini		+ changes\changes.ini + changes\changes-android-outro.ini		+ override.txt "Expanded Galaxy Main Installer\tslpatchdata\changes-android.ini"
-copy changes\changes-disc-intro.ini			+ changes\changes.ini + changes\changes-disc-outro.ini			+ override.txt "Expanded Galaxy Main Installer\tslpatchdata\changes-disc.ini"
-copy changes\changes-gog-intro.ini			+ changes\changes.ini + changes\changes-gog-outro.ini			+ override.txt "Expanded Galaxy Main Installer\tslpatchdata\changes-gog.ini"
-copy changes\changes-legacypc-intro.ini		+ changes\changes.ini + changes\changes-legacypc-outro.ini		+ override.txt "Expanded Galaxy Main Installer\tslpatchdata\changes-legacypc.ini"
-copy changes\changes-steam-intro.ini		+ changes\changes.ini + changes\changes-steam-outro.ini			+ override.txt "Expanded Galaxy Main Installer\tslpatchdata\changes-steam.ini"
-copy changes\changes-switch-intro.ini		+ changes\changes.ini + changes\changes-switch-outro.ini		+ override.txt "Expanded Galaxy Main Installer\tslpatchdata\changes-switch.ini"
-copy changes\changes-xbox-intro.ini			+ changes\changes.ini + changes\changes-xbox-outro.ini			+ override.txt "Expanded Galaxy Main Installer\tslpatchdata\changes-xbox.ini"
+type changes\changes-amazon-intro.ini		changes\changes.ini changes\changes-amazon-outro.ini		override.txt > "Expanded Galaxy Main Installer\tslpatchdata\changes-amazon.ini"
+type changes\changes-android-intro.ini		changes\changes.ini changes\changes-android-outro.ini		override.txt > "Expanded Galaxy Main Installer\tslpatchdata\changes-android.ini"
+type changes\changes-disc-intro.ini			changes\changes.ini changes\changes-disc-outro.ini			override.txt > "Expanded Galaxy Main Installer\tslpatchdata\changes-disc.ini"
+type changes\changes-gog-intro.ini			changes\changes.ini changes\changes-gog-outro.ini			override.txt > "Expanded Galaxy Main Installer\tslpatchdata\changes-gog.ini"
+type changes\changes-legacypc-intro.ini		changes\changes.ini changes\changes-legacypc-outro.ini		override.txt > "Expanded Galaxy Main Installer\tslpatchdata\changes-legacypc.ini"
+type changes\changes-steam-intro.ini		changes\changes.ini changes\changes-steam-outro.ini			override.txt > "Expanded Galaxy Main Installer\tslpatchdata\changes-steam.ini"
+type changes\changes-switch-intro.ini		changes\changes.ini changes\changes-switch-outro.ini		override.txt > "Expanded Galaxy Main Installer\tslpatchdata\changes-switch.ini"
+type changes\changes-xbox-intro.ini			changes\changes.ini changes\changes-xbox-outro.ini			override.txt > "Expanded Galaxy Main Installer\tslpatchdata\changes-xbox.ini"
 
 del override.txt
 
