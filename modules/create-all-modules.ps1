c1742378de604e893de6379e779d5e5c00734eb3
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2024-12-02 21:19:48 +0000

    Folder Names Updated

--- modules/create-all-modules.ps1
+++ modules/create-all-modules.ps1
@@ -4,7 +4,11 @@ foreach ($filename in $filenames)
 {
 	$folder = $filename.directoryname+"\"+$filename.basename
 	$file = $filename.fullname
-	$files = Get-ChildItem $folder | % { $_.FullName }
+	$name = $filename.basename
+	$directory = $filename.directoryname
+	$matching = Get-ChildItem $directory -Directory | Where-Object { $_.Name -match $name }
+	$match = $matching.fullname
+	$files = Get-ChildItem $match | % { $_.FullName }
 	& 'C:\Program Files (x86)\KotOR Tools\Xoreos Tools\erf' --mod --v10 $file $files
 }
 
