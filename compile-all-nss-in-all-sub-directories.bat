b3b444a6df89ae3305e39531a11c9b29cfd563cb
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2024-12-02 21:58:18 +0000

    compile scripts script updated

--- compile-all-nss-in-all-sub-directories.bat
+++ compile-all-nss-in-all-sub-directories.bat
@@ -10,4 +10,14 @@ for /r %%F in (./*.nss) do (
   )
 )
 
+for /r "Expanded Galaxy Main Installer\source\Override" %%x in (*.ncs) do move "%%x" "Expanded Galaxy Main Installer\tslpatchdata\Override"
+
+for /d %%f in (Expanded Galaxy Main Installer\source\Modules\*) do (
+	for /r %%i in (Expanded Galaxy Main Installer\source\Modules\%%f\*.ncs) do (
+		move "%%i" "Expanded Galaxy Main Installer\tslpatchdata\%%f"
+	)
+)
+
+echo new scripts must be added to the changes.ini for each module! for now...
+
 pause
