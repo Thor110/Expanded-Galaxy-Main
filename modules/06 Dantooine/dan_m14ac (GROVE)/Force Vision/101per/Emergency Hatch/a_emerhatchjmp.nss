22e23980816dfa4ece58b506226333a104073c3c
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2024-12-02 21:35:57 +0000

    Sub Folders Removed

--- modules/06 Dantooine/dan_m14ac (GROVE)/Force Vision/101per/Emergency Hatch/a_emerhatchjmp.nss
+++
@@ -1,6 +0,0 @@
-void main() {
-	object oWP_EMER_HATCH = GetObjectByTag("WP_EMER_HATCH", 0);
-	EnableVideoEffect(3);
-	AssignCommand(GetPCSpeaker(), ActionJumpToLocation(GetLocation(oWP_EMER_HATCH)));
-}
-
--- update-modules.bat
+++ update-modules.bat
@@ -2,19 +2,6 @@
 rem Thor110
 rem update all modules
 
-REM note
-
-echo NOTE : UPDATE THIS SCRIPT
-
-for /r "Modules Source" %%x in (*.mod) do copy "%%x" "%cd%\Expanded Galaxy Main Installer\tslpatchdata\Modules"
-
-echo Modules Updated
-pause
-exit
-
-REM TO ADD THIS
-REM REMOVE LEVEL NAMES FROM MODULE FOLDER NAMES AND REMOVE SUBDIRECTORIES
-
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%cd%\modules\create-all-modules.ps1'" 
 
 for /r "Modules Source" %%x in (*.mod) do copy "%%x" "%cd%\Expanded Galaxy Main Installer\tslpatchdata\Modules"
